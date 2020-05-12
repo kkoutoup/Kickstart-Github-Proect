603590a168687af7ea3614b0010aa70488a8028b
kkoutoup <kkoutoup@gmail.com>
2020-05-12 09:41:46 +0100

    Added language selection feature

--- kickstart_project.py
+++ kickstart_project.py
@@ -25,7 +25,7 @@ def get_project_name():
   # ask the user
   user_input = input("What's the name of the project? (use spaces to separate words): ").strip()
   # ask user to confirm
-  confirm = input('Please confirm project name is "{}" Y/N  '.format(user_input)).lower()
+  confirm = input("Please confirm project name is '{}' Y/N ".format(user_input)).lower()
   answers = ["y", "n"]
   # repeat ask for confirmation
   while confirm == "n" or confirm not in answers:
@@ -34,6 +34,19 @@ def get_project_name():
   else:
     project_name = user_input.title().replace(" ", "-")
     return project_name
+
+def select_language():
+  print("=> Select language")
+  languages = ["Python", "Ruby", "Javascript", "None"]
+  for index, language in enumerate(languages, start=1):
+    print(f'{index}: {language}')
+  user_input = int(input("What language will you be scripting in? Select index ").strip())
+  if user_input <= len(languages):
+    print(f'You selected {languages[user_input - 1]}')
+    input()
+  else:
+    print(f'Your list doesn\'t contain as many languages max = {len(languages)}')
+    select_language()
 
 def initialize_project():
   # create project folder and add README.md with
