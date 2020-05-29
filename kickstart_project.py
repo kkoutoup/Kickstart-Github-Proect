6c1cb813b13a09c4d2a0ee6b1d368285252e2fbe
kkoutoup@gmail.com <kkoutoup@gmail.com>
2020-05-30 00:26:23 +0100

    Removed see_project_list from main file

--- kickstart_project.py
+++ kickstart_project.py
@@ -9,16 +9,13 @@ from time import sleep
 # Read imports from folder
 sys.path.append('/home/kkoutoup/Projects/Kickstart-Github-Project/Dependencies')
 
-from Dependencies.see_project_list import see_project_list
 from Dependencies.project_name import set_project_name
 from Dependencies.select_language import select_language, set_language_extension
 from Dependencies.github_init import github_init
 
 def initialize_project():
   # show list of existing projects
-  see_project_list()
   # create project folder and coding file
-  # added a test comment
   project_name = set_project_name()
   os.mkdir(project_name)
   os.chdir(os.path.join(os.getcwd(), project_name))
